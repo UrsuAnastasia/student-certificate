2e0af6aae4191b257f49d0a563034870ea9e34aa
unknown <anastasia.ursu@assist.ro>
2023-05-18 09:45:49 +0300

    uninstaled antd chart

--- src/features/dashboard/components/pie-chart/pie-chart.tsx
+++ src/features/dashboard/components/pie-chart/pie-chart.tsx
@@ -1,53 +1,50 @@
-import { Pie } from '@ant-design/charts'
 import './pie-chart.scss'
 import { FC } from 'react'
 
 interface ILicense {
   title: string
 }
-const data = [
-  {
-    type: 'Calculatoare',
-    value: 27,
-  },
-  {
-    type: 'Electronica aplicata',
-    value: 25,
-  },
-  {
-    type: 'Reţele şi software de telecomunicaţii',
-    value: 18,
-  },
-  {
-    type: 'Sisteme electrice',
-    value: 18,
-  },
-  {
-    type: 'Energetică şi tehnologii informatice',
-    value: 18,
-  },
-  {
-    type: 'Managementul energiei',
-    value: 18,
-  },
-]
-const configLicense = {
-  appendPadding: 10,
-  data,
-  angleField: 'value',
-  colorField: 'type',
-  color: ['#ffea90', '#244eb6', '#967ae1', '#fdbace', '#982cc9', '#30a9fa'],
-  radius: 0.9,
-}
+// const data = [
+//   {
+//     type: 'Calculatoare',
+//     value: 27,
+//   },
+//   {
+//     type: 'Electronica aplicata',
+//     value: 25,
+//   },
+//   {
+//     type: 'Reţele şi software de telecomunicaţii',
+//     value: 18,
+//   },
+//   {
+//     type: 'Sisteme electrice',
+//     value: 18,
+//   },
+//   {
+//     type: 'Energetică şi tehnologii informatice',
+//     value: 18,
+//   },
+//   {
+//     type: 'Managementul energiei',
+//     value: 18,
+//   },
+// ]
+// const configLicense = {
+//   appendPadding: 10,
+//   data,
+//   angleField: 'value',
+//   colorField: 'type',
+//   color: ['#ffea90', '#244eb6', '#967ae1', '#fdbace', '#982cc9', '#30a9fa'],
+//   radius: 0.9,
+// }
 export const PieChart: FC<ILicense> = ({ ...props }: any) => {
   return (
     <div className='pie'>
       <div className='pie-header'>
         <h1 className='pie-title'>{props.title}</h1>
       </div>
-      <div className='pie-chart'>
-        <Pie height={300} {...configLicense} />
-      </div>
+      <div className='pie-chart'></div>
     </div>
   )
 }
